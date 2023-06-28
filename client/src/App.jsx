be409082009084caa50207f7d6e2d52066ad0447
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2023-06-28 23:54:11 +0300

    fix: routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
 import './App.sass'
 import Watchlist from './components/Watchlist';
@@ -9,19 +9,18 @@ import Movies from './components/Movies';
 function App() {
 
   return (
-	<BrowserRouter>
-		<Routes>
-			<Route index element={<Home/>} />
-			<Route path="watchlist" element={<Watchlist />} />
-			<Route path='movies' element={<Movies/>}>
-				<Route path=':id' element={<Title/>}/>
-			</Route>
-			<Route path='series'>
-				{/* <Route path=':id'element={<Title/>}/> */}
-			</Route>
-			<Route path="*" element={<NoMatch />} />
-		</Routes>
-	</BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+        <Route path="/movies" element={<Movies />}/>
+		<Route path="/movies/:id" element={<Title />} />
+        {/* <Route path="/series" element={<Series />}> */}
+          {/* <Route path=":id" element={<Title />} /> */}
+        {/* </Route> */}
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </Router>
   );
 }
 
